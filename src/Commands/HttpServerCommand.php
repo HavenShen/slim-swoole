d3efd7cdf79daf212a1621831d9aeb457ab55ff3
Haven Shen <havenshen@gmail.com>
2020-06-18 18:57:18 +0800

    src first

---
+++ src/Commands/HttpServerCommand.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace HavenShen\Slim\Swoole\Server;
+
+use Symfony\Component\Console\Command\Command;
+/**
+ * Manager
+ *
+ * @author    Haven Shen <havenshen@gmail.com>
+ * @copyright    Copyright (c) Haven Shen
+ */
+class HttpServerCommand extends Command
+{
+
+    public function __construct()
+    {
+        # code...
+    }
+}
---
+++ src/Server/Manager.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace HavenShen\Slim\Swoole\Server;
+
+/**
+ * Manager
+ *
+ * @author    Haven Shen <havenshen@gmail.com>
+ * @copyright    Copyright (c) Haven Shen
+ */
+class Manager
+{
+
+    public function __construct()
+    {
+        # code...
+    }
+
+    protected function start()
+    {
+        //
+    }
+}
